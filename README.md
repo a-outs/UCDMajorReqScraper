c7e4d893575d503548bf6be520cc17b2bb7d3888
Tim <timstewartj@gmail.com>
2021-01-26 20:24:37 -0800

    continued improvements, added prerequisite fetchin

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is a scraper for a potential major planner for UC Davis.
 In its current form, it latches on to the myDegree api to get data about major requirements. To do this, it needs what's called a bearer token. Ask Tim how to get your bearer token, and once you do get it, you want to put it a file called 'bearer.token' in the uppermost directory.
 
 Here's a screenshot of where it is if you can decipher it: https://imgur.com/qNK8B9q
+There is some sort of experation period for the token so you will have to get a new one if the current one no longer works.
+
+Currently all the script does is output only the major requirements of a desired major in addition to prerequisites for each class. This does not include GE or college requirements. To specify a major, use its code as the first and only argument upon running the script. A list of majors and their codes can be found in `listofmajors.txt`. If a major is not specified, it will default to CS.
+
+Output is directed both to the terminal and to a json file in `output/`.
--- majorreq.py
+++ majorreq.py
@@ -1,8 +1,12 @@
 import json
 import requests
+import sys
 
 raw = open("majors.json", "r")
 rawJson = json.load(raw)
+
+#for i in rawJson["_embedded"]["majors"]:
+#    print(i["description"])
 
 apiURL = "https://mydegree.ucdavis.edu/responsiveDashboard/api/audit"
 major = "major"
@@ -11,10 +15,21 @@ with open('bearer.token', 'r') as f:
     auth_token = f.readlines()[0]
 hed = {'Authorization': 'Bearer ' + auth_token}
 
-major = "LCSI"
+if(len(sys.argv) > 1): 
+    major = sys.argv[1]
+else:
+    major = "LCSI"
 params["goals"][0]["value"] = major
 response = requests.post(apiURL, json=params, headers=hed)
 responseJson = response.json()
+
+outputDict = {major : []}
+outputFile = open("output/" + major + ".json", "w")
+"""
+courseReqsRaw = requests.get("https://mydegree.ucdavis.edu/responsiveDashboard/api/course-link?discipline=ECS&number=036A&", headers=hed)
+courseReqs = courseReqsRaw.json()["courseInformation"]["courses"]
+print(courseReqs[0]["prerequisites"])
+"""
 
 for i in responseJson["blockArray"]:
     if i["requirementType"] == "MAJOR":
@@ -22,5 +37,31 @@ for i in responseJson["blockArray"]:
             if j["ruleType"] == "Subset":
                 for k in j["ruleArray"]:
                     if k["ruleType"] == "Course":
+                        print(k["label"])
+                        outputDict[major].append({k["label"] : []})
                         for l in k["requirement"]["courseArray"]:
-                            print(l["discipline"] + "-" + l["number"])
+                            if("numberEnd" in l):
+                                print("\t" + l["discipline"] + "-" + l["number"] + "-" + l["numberEnd"])
+                                outputDict[major][len(outputDict[major])-1][k["label"]].append({"course" : l["discipline"] + "-" + l["number"] + "-" + l["numberEnd"], "prerequisites" : ""})
+                            else:
+                                courseReqsRaw = requests.get("https://mydegree.ucdavis.edu/responsiveDashboard/api/course-link?discipline=" + l["discipline"] + "&number=" + l["number"] + "&", headers=hed)
+                                courseReqs = courseReqsRaw.json()["courseInformation"]["courses"]
+                                courseReqsString = ""
+                                if "prerequisites" in courseReqs[0]:
+                                    for m in courseReqs[0]["prerequisites"]:
+                                        courseReqsString += m["connector"] + " " + m["leftParenthesis"] + m["subjectCodePrerequisite"] + "-" + m["courseNumberPrerequisite"] + m["rightParenthesis"] + " "
+                                print("\t" + l["discipline"] + "-" + l["number"] + courseReqsString)
+                                outputDict[major][len(outputDict[major])-1][k["label"]].append({"course" : l["discipline"] + "-" + l["number"], "prerequisites" : courseReqsString})
+                        if("except" in k["requirement"]):
+                            print("\tEXCEPT:")
+                            outputDict[major][len(outputDict[major])-1][k["label"]].append({"EXCEPT":[]})
+                            for m in k["requirement"]["except"]["courseArray"]:
+                                if("numberEnd" in m):
+                                    print("\t\t" + m["discipline"] + "-" + m["number"] + "-" + m["numberEnd"])
+                                    outputDict[major][len(outputDict[major])-1][k["label"]][len(outputDict[major][len(outputDict[major])-1][k["label"]])-1]["EXCEPT"].append(m["discipline"] + "-" + m["number"] + "-" + m["numberEnd"])
+                                else:
+                                    print("\t\t" + m["discipline"] + "-" + m["number"])
+                                    outputDict[major][len(outputDict[major])-1][k["label"]][len(outputDict[major][len(outputDict[major])-1][k["label"]])-1]["EXCEPT"].append(m["discipline"] + "-" + m["number"])
+json.dump(outputDict, outputFile, indent=4)
+outputFile.close()
+raw.close()
